a5d22f078fd88274e350c9a55b98b79327949d17
amansinghbais <singh11amanbais@gmail.com>
2023-07-02 18:57:01 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useState } from "react"
+import axios from "axios"
+import Navbar from './component/Navbar';
+
 
 function App() {
+
+  const [state, setState] = useState({selectedFile: null,fileUploadedSuccessfully: false})
+
+
+  const onFileChange = (event) => {
+    setState({ selectedFile: event.target.files[0] });
+  }
+  
+  const onFileUpload = () => {
+    const formData = new FormData();
+    formData.append(
+      "demo file",
+      state.selectedFile,
+      state.selectedFile.name
+    )
+
+    // Call api
+    axios.post("https://4yxzp6cwg2.execute-api.us-east-1.amazonaws.com/fileUploaderFunction", formData).then(()=>{
+      setState({selectedFile: null});
+      setState({fileUploadedSuccessfully: true});
+    })
+
+    // console.log(formData)
+  }
+
+  function FileData(){
+    if (state.selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <div className='d-flex flex-column align-items-start'>
+          <p><span className='text-success fs-4'>File Name:</span> {state.selectedFile.name}</p>
+          <p><span className='text-success fs-4'>File Type:</span> {state.selectedFile.type}</p>
+          <p><span className='text-success fs-4'>Last Modified:</span> {" "}
+            {state.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+          </div>
+        </div>
+      )
+    } else if (state.fileUploadedSuccessfully) {
+      return (
+        <div>
+          <br />
+          <h4>Your file has been successfully uploaded.</h4>
+        </div>
+      )
+    } else{
+      return(
+        <div>
+          <br />
+          <h4>Choose a file and then press the upload button</h4>
+        </div>
+      )
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: '#c6c6c6', height: "100vh"}}>
+      <Navbar />
+      <h4 className='mt-2'>Select a file to upload to Amazon S3</h4>
+      <div className='border border-2'>
+        <input className='input' type="file" onChange={onFileChange} />
+        <button className='btn btn-success rounded-0' onClick={onFileUpload}>Upload</button>
+      </div>
+      <hr />
+      <FileData />
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-primary w-100">
+            <div className="container-fluid">
+                <a className="navbar-brand fs-3" href="#">-------- FileUploader --------</a>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
